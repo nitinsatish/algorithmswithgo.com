da426fcd5b181be16d35973ab18d9fff0e1c34bf
Nitin Satish <nitinsatish@gmail.com>
2020-03-29 13:05:55 +0530

    Decimal to base

--- module01/dec_to_base.go
+++ module01/dec_to_base.go
@@ -1,5 +1,7 @@
 package module01
 
+import "fmt"
+
 // DecToBase will return a string representing
 // the provided decimal number in the provided base.
 // This is limited to bases 2-16 for simplicity.
@@ -10,5 +12,11 @@ package module01
 //   DecToBase(14, 2) => "1110"
 //
 func DecToBase(dec, base int) string {
-	return ""
+	var result string
+	for dec > 0 {
+		rem := dec % base
+		result = fmt.Sprintf("%X", rem) + result
+		dec = dec / base
+	}
+	return result
 }
